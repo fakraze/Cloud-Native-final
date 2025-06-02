f7f9de979c3abd11453ad30a2418e9065331f67b
TSENG,SHAO-WEI <fakraze.cs11@nycu.edu.tw>
2025-06-03 00:00:26 +0800

    Ci cd, fix 2 url for product and develop (#8)
    * test cd using server on my pc
    
    * change ci, cd to EC2 k3s server
    
    * fix cd to SSH to EC2
    
    * fix cd ssh
    
    * debug: ssh to EC2
    
    * fix: make CD run on the same branch as CI
    
    * test, deleted main cd.yml
    
    * test: temporary run cd directly
    
    * fix cd
    
    * fix pull from github in EC2
    
    * add test in cd
    
    * add npm in EC2
    
    * fix cd
    
    * fix cd
    
    * fix cd
    
    * fix cd
    
    * fix cd
    
    * fix cd
    
    * fix k8s namesspace
    
    * fix k8s namespace
    
    * fix E2E test
    
    * fix EC2
    
    * fix EC2
    
    * fix frontend service to public
    
    * fix cypress test baseUrl
    
    * fix ghcr visibility
    
    * fix frontend service target port
    
    * Two namespace for product/develop app, only cd when push on main/develop, add README for Run k8s on yor own
    
    * add website link in README
    
    * merge ci cd to one file
    
    * fix ci_cd syntax
    
    * fix ci execute on any branch push
    
    * fix ci_cd use sed to replace local image
    
    * fix ci_cd to pull latest image
    
    * add cypress test, cd on ci-cd push
    
    * add cypress test, cd on ci-cd push
    
    * fix syntax
    
    * fix(ci): force clean EC2 repo before deployment to avoid git conflicts
    
    * fix cd, set correct image tag for ci-cd
    
    * cd, add checklist for pod
    
    * feat(ci): inject dynamic VITE_BASE_PATH based on branch for frontend Docker build
    
    * fix: enable process.env support in Vite config for base path injection
    
    * fix(vite): add Node types for process.env usage in vite.config.ts
    
    * fix(docker): update node version to 20 to support react-router v7
    
    * fix nginx to handle prod and dev
    
    * fix ingress-dev to add middle layer for correct url
    
    * fix nginx.conf for corrct url
    
    * add debug message in vite.config.js
    
    * add debug message in cd
    
    * delete middle layer in ingress-dev
    
    * add basename in App.tsx router
    
    * add debug message in App.tsx
    
    * add debug message in docker build
    
    * fix frontend docker VITE_BASE_PATH to variable
    
    * fix nginx.conf
    
    * fix nginx.conf
    
    * add middle layer in ingress
    
    * fix ingress-dev
    
    * fix nginx.conf
    
    * fix ssh due to new EC2 server
    
    * fix ssh in cd
    
    * fix ssh

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="node" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("📦 VITE_BASE_PATH =", process.env.VITE_BASE_PATH);
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
+
   plugins: [react()],
   server: {
     host: true,
