085a95d65ab0b084f48ec32970ad18e9b589094b
fakraze <fakraze.cs11@nycu.edu.tw>
2025-06-02 20:29:58 +0800

    add debug message in vite.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("📦 VITE_BASE_PATH =", process.env.VITE_BASE_PATH);
+
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || '/',
