be14d4b00ed3195f656aeb69d350a56d311403fa
fayian <iamfarmer1415@gmail.com>
2025-06-02 16:16:21 +0800

    feat: Connect to database
    Uses local mySQL database.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+require('dotenv').config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST!,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_DATABASE!,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
